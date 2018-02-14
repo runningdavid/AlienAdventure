8013b6644be5654f2697437f1a62d586b0e05638
runningdavid <runningdavid3@gmail.com>
2018-02-13 23:06:03 -0800

    Set canvas scale width/height param, reduce scroller speed

---
+++ Assets/Scripts/Menu/BackButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackButton : MonoBehaviour {
+
+    private void Awake()
+    {
+        float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
+        Vector3 bottomLeftPos = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distanceToCamera));
+        Vector3 buttonPos = bottomLeftPos + new Vector3(1.00f, 0.75f, 0);
+        transform.position = buttonPos;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
